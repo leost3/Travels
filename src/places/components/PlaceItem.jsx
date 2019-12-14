a520c324704da8ff65e4d6e6789f443831350e63
leost3 <leost3@gmail.com>
2019-12-14 14:43:46 -0500

    custom button element added and replaced on placeItem buttons

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import Card from "../../shared/components/UIElements/Card"
-import "./PlaceItem.css"
+import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
+import "./PlaceItem.css";
 
-const PlaceListItem = ({id,image,title,address,description}) => {
-    return <li className="place-item">
-        <Card className="place-item__content">
+const PlaceListItem = ({ id, image, title, address, description }) => {
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
         <div className="place-item__image">
-            <img src={image} alt={title} />
+          <img src={image} alt={title} />
         </div>
         <div className="place-item__info">
-            <h2>{title}</h2>
-            <h3>{address}</h3>
-            <p>{description}</p>
+          <h2>{title}</h2>
+          <h3>{address}</h3>
+          <p>{description}</p>
         </div>
-        <div className="place-item__actions" >
-            <button>VIEW ON MAP</button>
-            <button>EDIT</button>
-            <button>DELETE</button>
+        <div className="place-item__actions">
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
-        </Card>
+      </Card>
     </li>
-}
+  );
+};
 
-export default PlaceListItem
+export default PlaceListItem;
