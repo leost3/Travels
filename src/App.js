c96abd007785221e8d13882bc5c2c4b05cffbb81
leost3 <leost3@gmail.com>
2019-12-18 18:26:27 -0500

    new places form submit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Switch
 } from "react-router-dom";
 import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/Place";
+import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 
--- src/places/pages/Place.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewPlace = () => {
-  return <h2>New place work </h2>;
-};
-
-export default NewPlace;
